563e929d9cfd7b42fd63e4cbc8d0eefbc4460171
Jason Travis <JasonTravis@nau.edu>
2014-05-31 16:31:55 -0700

    Add prototype gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,74 @@
 'use strict';
 
 var path = {
-  scripts: [
-    '**/*.js',
-    '!js/lib/**',
-    '!node_modules/**',
-    '!bower_components/**'
-  ]
+  sass: [
+    ''
+  ],
+  script: [
+    '*.js',
+    'js/**/*.js',
+  ],
+  html: [
+    'popup.html'
+  ],
+  css: [
+    'css/*.css',
+    'bower_components/angular/angular-csp.css',
+    'bower_components/bootstrap/dist/css/bootstrap.css'
+  ],
+  dest: {
+    css: 'dist/style/'
+  }
 };
 
 var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
+var wiredep = require('wiredep').stream;
 
-gulp.task('default', ['jscs', 'jshint'], function() {
-  // place code for your default task here
+gulp.task('default', ['clean', 'jscs', 'jshint', 'uncss']);
+
+gulp.task('clean', function() {
+  return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
 });
 
-gulp.task('jscs', function() {
-  return gulp.src(path.scripts)
-  .pipe(plugins.jscs());
-});
-
+// Lint Javascript
 gulp.task('jshint', function() {
-  return gulp.src(path.scripts)
-  .pipe(plugins.jshint('.jshintrc'))
-  .pipe(plugins.jshint.reporter('jshint-stylish'));
+  return gulp.src(path.script)
+  .pipe($.jshint('.jshintrc'))
+  .pipe($.jshint.reporter('jshint-stylish'));
+});
+
+// Check Javascript code style
+gulp.task('jscs', function() {
+  return gulp.src(path.script)
+  .pipe($.jscs());
+});
+
+// Compile sass
+gulp.task('sass', function() {
+  gulp.src(path.sass)
+  .pipe($.sass())
+  .pipe(gulp.dest(path.css));
+});
+
+// Remove unused CSS rules
+gulp.task('uncss', ['sass'], function() {
+  return gulp.src(path.css)
+  .pipe($.uncss({
+    html: path.html, ignore: [], timeout: 0
+  }))
+  .pipe(gulp.dest(path.dist.css));
+});
+
+gulp.task('wiredep', function() {
+  gulp.src('popup.html')
+  .pipe(wiredep({
+    directory: 'bower_components'
+  }))
+  .pipe(gulp.dest(path.dist));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(path.scripts, ['jshint', 'jscs']);
+  gulp.watch(path.script, ['jshint', 'jscs']);
+  gulp.watch(path.sass, ['sass']);
 });
